3ba4f994ef015bf51b01b2c7ec254f084eca8ded
Yohai1309 <yohai1309@gmail.com>
2022-06-16 16:05:35 +0300

    Changed Port.txt location

--- simon-controller.py
+++ simon-controller.py
@@ -2,13 +2,15 @@ import socket
 import os
 
 # Controlling center of Simon game
-os.system("rm ~/Desktop/port.txt")
-os.system("sshpass -p 'Aa123456' scp pi@192.168.0.42:/home/pi/Desktop/simon-py/port.txt ~/Desktop")
+os.system("rm ./port.txt")
+os.system("sshpass -p 'Aa123456' scp pi@192.168.0.42:/home/pi/Desktop/simon-py/port.txt ./port.txt")
 
 RP_IP = '192.168.0.42'
 
+f = open("./port.txt", 'r')
+port = int(f.readline())
+print(port)
 # CHANGE THE PORT TO THE PORT APPEARING ON THE SCREEN
-PORT = 53826
 
 # There are 4 different types of messages to send:
 #   'K' - Success (continuing to the next shape)
@@ -17,7 +19,7 @@ PORT = 53826
 #   'Q' - Quit (quiting game)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((RP_IP, PORT))
+sock.connect((RP_IP, port))
 while True:
     msg = input("Enter command: ")
     sock.sendall(msg.encode())
