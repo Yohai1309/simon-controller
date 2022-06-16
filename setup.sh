e031719bb0b0fd2ad9633e417a3d79b9aa94ebe2
Yohai1309 <yohai1309@gmail.com>
2022-06-16 13:11:24 +0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sshpass -p 'raspberry' ssh pi@192.168.0.42 'sudo reboot'
---
+++ simon-controller.py
@@ -0,0 +1,22 @@
+import socket
+
+# Controlling center of Simon game
+
+RP_IP = '192.168.0.42'
+
+# CHANGE THE PORT TO THE PORT APPEARING ON THE SCREEN
+PORT = 52237
+
+# There are 4 different types of messages to send:
+#   'K' - Success (continuing to the next shape)
+#   'F' - Failure (restarting level)
+#   'R' - Restart (restarting game)
+#   'Q' - Quit (quiting game)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((RP_IP, PORT))
+while True:
+    msg = input("Enter command: ")
+    sock.sendall(msg.encode())
+    if msg == 'Q':
+        break
