4710512acaa94294d3aa26d53a63b219c3bc2bf5
Yohai1309 <yohai1309@gmail.com>
2022-06-16 16:11:47 +0300

    Changed Port.txt location

--- simon-controller.py
+++ simon-controller.py
@@ -1,15 +1,7 @@
 import socket
 import os
-
 # Controlling center of Simon game
-os.system("rm ./port.txt")
-os.system("sshpass -p 'Aa123456' scp pi@192.168.0.42:/home/pi/Desktop/simon-py/port.txt ./port.txt")
 
-RP_IP = '192.168.0.42'
-
-f = open("./port.txt", 'r')
-port = int(f.readline())
-print(port)
 # CHANGE THE PORT TO THE PORT APPEARING ON THE SCREEN
 
 # There are 4 different types of messages to send:
@@ -18,6 +10,20 @@ print(port)
 #   'R' - Restart (restarting game)
 #   'Q' - Quit (quiting game)
 
+RP_IP = '192.168.0.42'
+port = None #<- Change this!
+
+def get_port():
+    os.system("rm ./port.txt")
+    os.system("sshpass -p 'Aa123456' scp pi@192.168.0.42:/home/pi/Desktop/simon-py/port.txt ./port.txt")
+
+    f = open("./port.txt", 'r')
+    port = int(f.readline())
+    print(port)
+    return port
+
+if port is None:
+    port = get_port()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((RP_IP, port))
 while True:
